dc97aa1f4ad8d95b7a8260b1354c9f2ece551020
이주연(Juyeon Lee)/Data Solution개발팀/SKP <jylee14@sk.com>
2026-01-15 13:29:16 +0900

    1.14.3

--- Package.swift
+++ Package.swift
@@ -14,22 +14,20 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", .upToNextMajor(from: "3.23.0")),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.0.0"))
+        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", .upToNextMajor(from: "3.23.0"))
     ],
     targets: [
         .target(
             name: "PlanetAdSDKTarget",
             dependencies: [
                 "PlanetAdSDK",
-                .product(name: "GoogleInteractiveMediaAds", package: "swift-package-manager-google-interactive-media-ads-ios"),
-                .product(name: "SDWebImage", package: "SDWebImage")
+                .product(name: "GoogleInteractiveMediaAds", package: "swift-package-manager-google-interactive-media-ads-ios")
             ]
         ),
         .binaryTarget(
             name: "PlanetAdSDK",
-            url: "https://storage.googleapis.com/planetad/ios_spm/v1.14.2/PlanetAdSDK.zip",
-            checksum: "502dc5898c4ce34a63f2a6d3b9208d10b32f842baf1437d48f440d216ad68b34"
+            url: "https://storage.googleapis.com/planetad/ios_spm/v1.14.3/PlanetAdSDK.zip",
+            checksum: "3325c3a4500c32d207a6991a29eb4af78861b365115c26fb22e50b9ac949f437"
         )
     ]
 )
