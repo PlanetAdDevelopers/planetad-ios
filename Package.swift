0885428bab3fcf9a13067e389a28c95e2152f956
이주연(Juyeon Lee)/Data Solution개발팀/SKP <jylee14@sk.com>
2026-04-21 13:07:39 +0900

    1.15.3

--- Package.swift
+++ Package.swift
@@ -26,8 +26,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PlanetAdSDK",
-            url: "https://storage.googleapis.com/planetad/ios_spm/v1.15.2/PlanetAdSDK.zip",
-            checksum: "7496879e84ee584ac15748da5d983951fa8dbbe0cf6aeaef891d2616e86734e1"
+            url: "https://storage.googleapis.com/planetad/ios_spm/v1.15.3/PlanetAdSDK.zip",
+            checksum: "48ecc2663b08a8ff57c634853d40c3d7cfaa68579f7b1de6c40ed89684aee28d"
         )
     ]
 )
