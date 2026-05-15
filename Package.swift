5db041209a596aadd93ec59fbfb22e7d77625986
이주연(Juyeon Lee)/Data Solution개발팀/SKP <jylee14@sk.com>
2026-05-15 14:18:43 +0900

    1.15.4

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", exact: "3.23.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-interactive-media-ads-ios", exact: "3.31.0")
     ],
     targets: [
         .target(
@@ -26,8 +26,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "PlanetAdSDK",
-            url: "https://storage.googleapis.com/planetad/ios_spm/v1.15.3/PlanetAdSDK.zip",
-            checksum: "48ecc2663b08a8ff57c634853d40c3d7cfaa68579f7b1de6c40ed89684aee28d"
+            url: "https://storage.googleapis.com/planetad/ios_spm/v1.15.4/PlanetAdSDK.zip",
+            checksum: "ce668156d32c42d52b466819dbe75b9edf12e6ab61e7ce803cf2dc5b52b19c66"
         )
     ]
 )
